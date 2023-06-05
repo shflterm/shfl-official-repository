5f43b164ab5fa507ca820190bbabd93cd63bec2f
김시후 <me@iamsihu.wtf>
2023-06-05 23:28:06 +0900

    Update entrypoint.lua

--- list/lib/entrypoint.lua
+++ list/lib/entrypoint.lua
@@ -1,7 +1,7 @@
 function entrypoint()
     for _, file in pairs(fs.list(workspace.dir)) do
         if fs.isDir(file) then
-            print("[44m"..file.."/")
+            print("[44m"..file.."/[0m")
         else
             print(file)
         end
