55a41e5316def423b71d4a5d7f3317da7263ba51
ssyuk <sihu@syuk.me>
2023-11-20 16:43:14 +0900

    [textutilities] py script로 업데이트

---
+++ textutilities/lib/capitalize/command.py
@@ -0,0 +1,4 @@
+def entrypoint(ws, options, bg_mode):
+    text = options["text"].capitalize()
+    print(text)
+    return text
--- textutilities/lib/input/command.lua
+++
@@ -1,5 +0,0 @@
-function entrypoint()
-    s = io.read("*l")
-    info(s)
-    return s
-end
