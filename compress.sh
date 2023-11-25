a270cf8f1362023ae61dff131426f4a564c9a501
ssyuk <sihu@syuk.me>
2023-11-25 22:49:52 +0900

    actions setup

--- compress.sh
+++ compress.sh
@@ -1,8 +1,8 @@
 cmake -S . -B build/
 cmake --build build
 
-rm -rf temp-$1
-mkdir temp-$1
+rm -rf temp
+mkdir temp
 cd build/$1
 
 os_type=$(uname -s)
@@ -10,18 +10,17 @@ os_type=$(uname -s)
 echo "Current OS: $os_type"
 
 if [ "$os_type" == "Darwin" ]; then
-  find . -name '*.dylib' -exec sh -c "cp --parents "{}" ../../temp-$1" \;
-  find ../../temp-$1 -type f -name '*.dylib' -exec sh -c 'mv "{}" "$(dirname "{}")/command.dylib"' \;
+  find . -name '*.dylib' -exec sh -c 'mkdir -p ../../temp-$1/"$(dirname "{}")" && cp "{}" ../../temp-$1/"$(dirname "{}")/command.dylib"' \;
   os=macos
 elif [ "$os_type" == "Linux" ]; then
-  find . -name '*.so' -exec sh -c "cp --parents "{}" ../../temp-$1" \;
-  find ../../temp-$1 -type f -name '*.so' -exec sh -c 'mv "{}" "$(dirname "{}")/command.so"' \;
+  find . -name '*.so' -exec sh -c "cp --parents "{}" ../../temp" \;
+  find ../../temp -type f -name '*.so' -exec sh -c 'mv "{}" "$(dirname "{}")/command.so"' \;
   os=ubuntu
 fi
 
-cp $1/app.shfl ../../temp-$1/
+cp $1/app.shfl ../../temp/
 
-cd ../../temp-$1
+cd ../../temp
 zip -r $1.zip *
 mv $1.zip ../$1_$os.shflapp
 cd ..
